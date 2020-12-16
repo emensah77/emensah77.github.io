03bc60fb0bc3f4e6cd437e9619019626f1f592a6
emensah77 <40795962+emensah77@users.noreply.github.com>
2020-12-16 11:49:02 -0800

    Updates

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky");
         }
-    })
+    });
+    //toogle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+    });
 });
